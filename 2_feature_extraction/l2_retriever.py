91a5ce76c9158297d9ac9fedfc35826ce74077ff
Denis <gaphex@gmail.com>
2019-07-02 22:53:18 +0300

    add method to precompute l2 norm to retriever

--- 2_feature_extraction/l2_retriever.py
+++ 2_feature_extraction/l2_retriever.py
@@ -12,18 +12,20 @@ class L2Retriever:
         )
         self.session = tf.Session(config=config)
         
-        self.norm = None
-        self.query = tf.placeholder("float", [self.dim])
-        self.kbase = tf.placeholder("float", [None, self.dim])
+        self.dtype = "float32"
+        
+        self.query = tf.placeholder(self.dtype, [self.dim])
+        self.kbase = tf.placeholder(self.dtype, [None, self.dim])
+        if self.use_norm:
+            self.norm = tf.placeholder(self.dtype, [None, 1])
+        else:
+            self.norm = None
         
         self.build_graph()
 
     def build_graph(self):
-      
-        if self.use_norm:
-            self.norm = tf.placeholder("float", [None, 1])
 
-        distance = euclidean_distances(self.kbase, self.query, self.norm)
+        distance = self.euclidean_distances(self.kbase, self.query, self.norm)
         top_neg_dists, top_indices = tf.math.top_k(tf.negative(distance), k=self.top_k)
         top_dists = tf.negative(top_neg_dists)
 
@@ -41,17 +43,26 @@ class L2Retriever:
         
         return I, D
       
-def euclidean_distances(kbase, query, norm=None):
-    query = tf.reshape(query, (1, -1))
+    @staticmethod
+    def euclidean_distances(kbase, query, norm=None):
+        query = tf.reshape(query, (1, -1))
+
+        if norm is None:
+          XX = tf.keras.backend.batch_dot(kbase, kbase, axes=1)
+        else:
+          XX = norm
+        YY = tf.matmul(query, tf.transpose(query))
+        XY = tf.matmul(kbase, tf.transpose(query))
+
+        distance = XX - 2 * XY + YY
+        distance = tf.sqrt(tf.reshape(distance, (-1,)))
+
+        return distance
     
-    if norm is None:
-      XX = tf.keras.backend.batch_dot(kbase, kbase, axes=1)
-    else:
-      XX = norm
-    YY = tf.matmul(query, tf.transpose(query))
-    XY = tf.matmul(kbase, tf.transpose(query))
-    
-    distance = XX - 2 * XY + YY
-    distance = tf.sqrt(tf.reshape(distance, (-1,)))
-    
-    return distance
+    @staticmethod
+    def compute_squared_l2_norm(mat):
+        square_norms = np.sum(mat**2, axis=1, keepdims=True)
+        return square_norms
+      
+    def __call__(self, kbase, query, norm=None):
+        return self.predict(kbase, query, norm)
