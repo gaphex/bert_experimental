e0b64a21aaa5fa0a36862e639f230f2f9de0fc7d
Denis <gaphex@gmail.com>
2019-07-09 14:44:24 +0300

    added support for multiple vector queries

--- 2_feature_extraction/l2_retriever.py
+++ 2_feature_extraction/l2_retriever.py
@@ -3,18 +3,19 @@ import numpy as np
 
 
 class L2Retriever:
-    def __init__(self, dim, top_k=3, use_norm=False, use_gpu=True):
+    def __init__(self, dim, top_k=3, use_norm=False, use_gpu=False):
+
         self.dim = dim
         self.top_k = top_k
         self.use_norm = use_norm
         config = tf.ConfigProto(
             device_count={'GPU': (1 if use_gpu else 0)}
         )
+        config.gpu_options.allow_growth = True
         self.session = tf.Session(config=config)
-        
         self.dtype = "float32"
         
-        self.query = tf.placeholder(self.dtype, [self.dim])
+        self.query = tf.placeholder(self.dtype, [None, self.dim])
         self.kbase = tf.placeholder(self.dtype, [None, self.dim])
         if self.use_norm:
             self.norm = tf.placeholder(self.dtype, [None, 1])
@@ -25,18 +26,20 @@ class L2Retriever:
 
     def build_graph(self):
 
-        distance = self.euclidean_distances(self.kbase, self.query, self.norm)
-        top_neg_dists, top_indices = tf.math.top_k(tf.negative(distance), k=self.top_k)
-        top_dists = tf.negative(top_neg_dists)
+        self.distance = self.euclidean_distances(self.kbase, self.query, self.norm)
+        top_neg_dists, top_indices = tf.math.top_k(
+            tf.negative(self.distance), k=self.top_k)
+        top_dists = tf.sqrt(tf.abs(tf.negative(top_neg_dists)))
 
         self.top_distances = top_dists
         self.top_indices = top_indices
 
     def predict(self, kbase, query, norm=None):
-        query = np.squeeze(query)
+
+        query = query.reshape((-1, self.dim))
         feed_dict = {self.query: query, self.kbase: kbase}
         if self.use_norm:
-          feed_dict[self.norm] = norm
+            feed_dict[self.norm] = norm
         
         I, D = self.session.run([self.top_indices, self.top_distances],
                                 feed_dict=feed_dict)
@@ -45,17 +48,17 @@ class L2Retriever:
       
     @staticmethod
     def euclidean_distances(kbase, query, norm=None):
-        query = tf.reshape(query, (1, -1))
 
         if norm is None:
-          XX = tf.keras.backend.batch_dot(kbase, kbase, axes=1)
+            XX = tf.keras.backend.batch_dot(kbase, kbase, axes=1)
         else:
-          XX = norm
-        YY = tf.matmul(query, tf.transpose(query))
+            XX = norm
+
+        YY = tf.transpose(tf.keras.backend.batch_dot(query, query, axes=1))
         XY = tf.matmul(kbase, tf.transpose(query))
 
         distance = XX - 2 * XY + YY
-        distance = tf.sqrt(tf.reshape(distance, (-1,)))
+        distance = tf.transpose(distance)
 
         return distance
     
