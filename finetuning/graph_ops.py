93bd3a17f937df6e0a2b3e4e43a2124c8e050746
gaphex <gaphex@gmail.com>
2019-11-30 22:02:08 +0300

    removed redundant import

--- finetuning/graph_ops.py
+++ finetuning/graph_ops.py
@@ -4,37 +4,6 @@ from tensorflow.python.framework.graph_util import convert_variables_to_constant
 from tensorflow.python.tools.optimize_for_inference_lib import optimize_for_inference
 
 
-def freeze_session(session, keep_var_names=None, output_names=None, clear_devices=True):
-    """
-    Freezes the state of a session into a pruned computation graph.
-
-    Creates a new computation graph where variable nodes are replaced by
-    constants taking their current value in the session. The new graph will be
-    pruned so subgraphs that are not necessary to compute the requested
-    outputs will be removed.
-    @param session The TensorFlow session to be frozen.
-    @param keep_var_names A list of variable names that should not be frozen,
-                          or None to freeze all the variables in the graph.
-    @param output_names Names of the relevant graph outputs.
-    @param clear_devices Remove the device directives from the graph for better portability.
-    @return The frozen graph definition.
-    """
-
-    graph = session.graph
-    with graph.as_default():
-        freeze_var_names = list(set(v.op.name for v in tf.global_variables()).difference(keep_var_names or []))
-        output_names = output_names or []
-        output_names += [v.op.name for v in tf.global_variables()]
-        # Graph -> GraphDef ProtoBuf
-        input_graph_def = graph.as_graph_def()
-        if clear_devices:
-            for node in input_graph_def.node:
-                node.device = ""
-        frozen_graph = tf.compat.v1.convert_variables_to_constants(session, input_graph_def,
-                                                                   output_names, freeze_var_names)
-        return frozen_graph
-
-
 def load_graph(frozen_graph_filename):
     with tf.gfile.GFile(frozen_graph_filename, "rb") as f:
         graph_def = tf.GraphDef()
@@ -53,8 +22,6 @@ def freeze_keras_model(model, export_path=None, clear_devices=True):
     @param clear_devices Remove the device directives from the graph for better portability.
     @return The frozen graph definition.
     """
-    from tensorflow.python.framework.graph_util import convert_variables_to_constants
-    from tensorflow.python.tools.optimize_for_inference_lib import optimize_for_inference
     
     sess = tf.keras.backend.get_session()
     graph = sess.graph
@@ -66,8 +33,6 @@ def freeze_keras_model(model, export_path=None, clear_devices=True):
         dtypes = [t.dtype.as_datatype_enum for t in input_tensors]
         input_ops = [t.name.rsplit(":", maxsplit=1)[0] for t in input_tensors]
         output_ops = [t.name.rsplit(":", maxsplit=1)[0] for t in output_tensors]
-        
-        print(input_ops, output_ops)
         
         tmp_g = graph.as_graph_def()
         if clear_devices:
